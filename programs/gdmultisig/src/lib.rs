0a23dbc8ba8336159b9a5d622d83edba168f61f5
alphaprime8 <alphaprime8888@gmail.com>
2022-12-18 21:46:44 +0800

    deployed to mainnet

--- programs/gdmultisig/src/lib.rs
+++ programs/gdmultisig/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2omcykYnUGQW8tDGKZFMuJHAswrfMDAgMTkBo3Kd6Woj");
 
 // consts
 pub const MAX_WITHDRAW_VALUE_MONTHLY_USD: f64 = 25_000.;
